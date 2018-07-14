e1a6aabe76251c164419a985b722a6c321a94f8d
Jamie Nicol <jamie.nicol@mail.utoronto.ca>
2018-07-14 11:28:11 -0400

    Create 709 - ToLowerCase.py

---
+++ 709 - ToLowerCase.py
@@ -0,0 +1,13 @@
+# Solution to LeetCode problem 709 - To Lower Case
+# Write a function that takes an input a string and returns the string in lowercase
+
+# example: toLowerCase("Hello") = "hello"
+
+class Solution:
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
+        
